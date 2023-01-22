4ee261ab661c6d4431b57a3cc4037b64279279a1
coddedwells <wells.mail.13@gmail.com>
2023-01-22 12:40:34 +0300

    updated Loggers

--- Logger.ts
+++ Logger.ts
@@ -1,8 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
 import chalk from 'chalk';
 
 export default class Logger {
     public static log = (args: any) => this.info(args);
     public static info = (args: any) => console.log(chalk.blue(`[${new Date().toLocaleString()}] [INFO]`), typeof args === 'string' ? chalk.blueBright(args) : args);
-    public static warning = (args: any) => console.log(chalk.yellow(`[${new Date().toLocaleString()}] [INFO]`), typeof args === 'string' ? chalk.yellowBright(args) : args);
-    public static error = (args: any) => console.log(chalk.red(`[${new Date().toLocaleString()}] [INFO]`), typeof args === 'string' ? chalk.redBright(args) : args);
+    public static warning = (args: any) => console.log(chalk.yellow(`[${new Date().toLocaleString()}] [WARN]`), typeof args === 'string' ? chalk.yellowBright(args) : args);
+    public static error = (args: any) => console.log(chalk.red(`[${new Date().toLocaleString()}] [ERROR]`), typeof args === 'string' ? chalk.redBright(args) : args);
+
+    public static logging = (req: Request, res: Response, next: NextFunction) => {
+        /** Log the request */
+        Logger.info(`Incomming -> Method: [${req.method}] - URL: [${req.url}] - IP:${req.socket.remoteAddress}`);
+
+        res.on('finish', () => {
+            /** Add status code */
+            Logger.info(`Incomming -> Method: [${req.method}] - URL: [${req.url}] - Status:[${res.statusCode}]`);
+        });
+
+        next();
+    };
 }
