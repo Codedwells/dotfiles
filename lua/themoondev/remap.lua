bb3e31ec5a543c339c51152f9c6dfeaf2e4e28c2
codedwells <wells.mail.13@gmail.com>
2023-09-03 14:50:53 +0300

    first commit

---
+++ lua/themoondev/remap.lua
@@ -0,0 +1,54 @@
+-- Author: Abel Misiocha
+-- Date: 2023-09-03
+
+local opts = { noremap = true, silent = true }
+
+-- Shorten function name
+local keymap = vim.keymap.set
+
+-- Set leader key
+vim.g.mapleader = " "
+
+-- Open explorer
+keymap("n", "<leader>e", vim.cmd.Ex)
+
+-- Normal --
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Close buffers
+keymap("n", "<leader>c", ":bdelete<CR>", opts)
+
+-- Writting a file
+keymap("n", "<leader>w", ":w<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Insert --
+-- Press jk fast to exit insert mode
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv^", opts)
+keymap("v", ">", ">gv^", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Formating
+keymap("n", "<leader>f", "<cmd> lua vim.lsp.buf.format()<CR>", opts)
